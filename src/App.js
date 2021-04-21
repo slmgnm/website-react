268c836c078a73398e079215208f13f1321b863b
Salim Ghanem <saleemghanem@gmail.com>
2021-04-21 13:07:39 +0300

    movieDetail page style route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 
 import { Route, Switch } from "react-router-dom";
+import MovieDetails from './movieDetails';
 function App() {
   return (
     <div className="App">
@@ -20,8 +21,11 @@ function App() {
         <Route path="/contact">
           <ContactUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetails />
         </Route>
       </Switch>
     </div>
