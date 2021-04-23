c45d2f86d36cd7e5786fe5b5e24bc3f05f283836
Salim Ghanem <saleemghanem@gmail.com>
2021-04-23 12:50:01 +0300

    ourWork animation

--- src/animation.js
+++ src/animation.js
@@ -14,7 +14,7 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y: 300,
+
     transition: {
       duration: 0.5,
     },
@@ -39,7 +39,28 @@ export const photoAnim = {
   show: {
     scale: 1,
     opacity: 1,
-
-    transition: { ease: "easeOut", duration: 0.75 },
+    transition: {
+      ease: "easeOut",
+      duration: 0.75,
+    },
   },
+};
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+export const lineAnim = {
+  hidden: { width: "0%" },
+  show: {
+    width: "100%",
+    transition: { duration: 1 },
+  },
+};
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,7 +7,14 @@ import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
 import { motion } from "framer-motion";
 
-import { pageAnimation } from "./../animation";
+import {
+  sliderContainer,
+  slider,
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+} from "./../animation";
 
 const OurWork = () => {
   return (
@@ -18,10 +25,18 @@ const OurWork = () => {
       exit="exit"
       style={{ background: "#fff" }}>
       <Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.div variants={sliderContainer}>
+          <Frame1 variants={slider}></Frame1>
+          <Frame2 variants={slider}></Frame2>
+          <Frame3 variants={slider}></Frame3>
+          <Frame4 variants={slider}></Frame4>
+        </motion.div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-          <img src={athlete} alt="athlete" />
+        <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="athlete" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
@@ -51,11 +66,14 @@ const Work = styled(motion.div)`
     padding: 1rem 0rem;
   }
 `;
+const Hide = styled.div`
+  overflow: hidden;
+`;
 const Movie = styled.div`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
-    background: #cccccc;
+    background: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -64,5 +82,26 @@ const Movie = styled.div`
     object-fit: cover;
   }
 `;
+//Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
+`;
+
+
 
 export default OurWork;
